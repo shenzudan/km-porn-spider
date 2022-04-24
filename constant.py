f5fc0db9347ffa781ffc998b8e09abb47d6e719a
stan <admin@stanwind.com>
2022-04-24 18:02:45 +0800

    1. refine code.

--- constant.py
+++ constant.py
@@ -1,11 +1,11 @@
 #define km constant
-#main endpoint
-import json
-
 import yaml
 import requests
 
-f = open('config.yml', 'r', encoding='utf-8')
+#config file path
+cfg_name = 'config.yml'
+
+f = open(cfg_name, 'r', encoding='utf-8')
 cfg = yaml.load(f.read(), Loader=yaml.FullLoader)
 
 def fetchDirectEndpoint():
@@ -16,26 +16,18 @@ def fetchDirectEndpoint():
         end = response.headers['Location']
         cfg['endpoint'] = end
         print('解析到新地址: %s' % end)
+        # record new address
+        # with open('output.yaml', 'w') as fp:
+        #     yaml.dump(cfg, fp)
 
 fetchDirectEndpoint()
 
 ENDPOINT = cfg['endpoint'] + "{0}"
-URL_ALL = "/api/videos/listAll"
-URL_HOT = "/api/videos/listHot"
-URL_DETAIL = "/api/videos/detail"
+URL_ALL = ENDPOINT.format("/api/videos/listAll")
+URL_HOT = ENDPOINT.format("/api/videos/listHot")
+URL_DETAIL = ENDPOINT.format("/api/videos/detail")
 
 SIG_KEY = "maomi_pass_xyz"
 AES_KEY = b"625202f9149maomi"
 AES_IV  = b"5efd3f6060emaomi"
-
-
-
-def getAllUrl():
-    return ENDPOINT.format(URL_ALL)
-
-def getHotUrl():
-    return ENDPOINT.format(URL_HOT)
-
-def getDetailUrl():
-    return ENDPOINT.format(URL_DETAIL)
 
